e322a0caf259fe8292c6b730dff900a224bcc844
kainingy <kainingy@uci.edu>
2015-01-30 15:02:44 -0800

    update

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.contrib.sites.models import Site
 
 # Create your models here.
 
@@ -8,6 +10,9 @@ class Post(models.Model) :
 	date_time = models.DateTimeField(auto_now_add = True)
 	content = models.TextField(blank = True, null = True)
 
-	def __unicode__(self) : 
-		return self.title
+	def __str__(self):
+    		return self.title.encode('utf-8')
+
+  	class Meta:
+        	ordering = ['-date_time']
 		
